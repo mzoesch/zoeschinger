0b8ee08340a25829b2f2b8b8a5c7ba6e2442aba7
mzoesch <magnus.zoeschinger@zoeschinger.de>
2023-02-27 21:05:23 +0100

    fresh start - tailwindcss is not used anymore

--- src/components/svg/index.tsx
+++
@@ -1,3 +0,0 @@
-export { default as LabIcon } from '@pub/svg/LabIcon.svg';
-export { default as DashboardIcon } from '@pub/svg/DashboardIcon.svg';
-export { default as AtEmailIcon } from '@pub/svg/AtEmailIcon.svg';
