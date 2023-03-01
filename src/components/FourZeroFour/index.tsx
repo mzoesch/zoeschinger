5a5dc10242cae666b9f3e700c5e6ac3b616d97d7
mzoesch <magnus.zoeschinger@zoeschinger.de>
2023-03-01 19:31:42 +0100

    404 patch

--- src/components/FourZeroFour/index.tsx
+++ src/components/FourZeroFour/index.tsx
@@ -1,4 +1,5 @@
 import styles from '@s/FourZeroFour/main.module.scss';
+import Link from 'next/link';
 
 import { GetCurrentRoute } from '@l/Routes';
 import { Quicksand } from 'next/font/google';
@@ -11,12 +12,36 @@ const Page = () => {
   return (
     <>
       <div className={styles.main}>
-        <h1 className={quicksand.className}>Whooooops</h1>
+        <div className={styles.whooooops}>
+          <h1 className={quicksand.className}>Whooooops</h1>
+        </div>
         <div className={styles.sub}>
           <h2>
             It looks like <span className={styles.magic_text}>{route}</span>{' '}
             went missing in the matrix 😖
           </h2>
+        </div>
+        <div className={styles.footer}>
+          <div className={styles.footer_text}>
+            Learn more about{' '}
+            <span>
+              <Link
+                className={styles.footer_link}
+                href={'https://en.wikipedia.org/wiki/HTTP_404'}
+                target='_blank'
+              >
+                error 404
+              </Link>
+            </span>
+          </div>
+          <div className={styles.footer_text}>
+            <Link
+              className={styles.footer_link}
+              href={'https://www.youtube.com/watch?v=dQw4w9WgXcQ'}
+            >
+              Take me back, where I belong
+            </Link>
+          </div>
         </div>
       </div>
     </>
