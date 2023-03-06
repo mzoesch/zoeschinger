73b4c399bfaa44ad25f66af24a45ca88b9dd0b5e
mzoesch <magnus.zoeschinger@zoeschinger.de>
2023-03-06 15:17:53 +0100

    cards align

--- src/components/Projects/ProjectCard.tsx
+++ src/components/Projects/ProjectCard.tsx
@@ -1,26 +1,34 @@
 import styles from '@s/projects/cards.module.scss';
+import Image from 'next/image';
 
-import { GitHubLogo, LinkIcon } from '@c/svg';
+import { GitHubLogo, LinkIcon, DashboardIcon } from '@c/svg';
 
 const ProjectCard = ({
   title,
   subText,
   href,
-}: {
+}: // img,
+{
   title: any;
   subText: any;
   href: any;
+  // img: any;
 }) => {
   return (
     <>
       <div className={styles.card}>
-        <div className={styles.title}>{title}</div>
-        <div className={styles.subText}>{subText}</div>
-        <div className={styles.footer}>
-          <div>Read more -D</div>
-          <div className={styles.links}>
-            <GitHubLogo className={styles.icons} />
-            <LinkIcon className={styles.icons} />
+        <div className={styles.top_img_wrapper}>
+          <DashboardIcon className={styles.top_img} />
+        </div>
+        <div className={styles.bottom}>
+          <div className={styles.title}>{title}</div>
+          <div className={styles.subText}>{subText}</div>
+          <div className={styles.footer}>
+            <div>Read more -D</div>
+            <div className={styles.links}>
+              <GitHubLogo className={styles.icons} />
+              <LinkIcon className={styles.icons} />
+            </div>
           </div>
         </div>
       </div>
