3b05d25254ddcb0128a2f9da82097be4256a17f1
Magnus Zoeschinger <magnus.zoeschinger@zoeschinger.de>
2023-03-10 15:41:09 +0100

    Trunk (#12)
    * Bump next from 13.2.1 to 13.2.3 in /next (#7)
    
    Bumps [next](https://github.com/vercel/next.js) from 13.2.1 to 13.2.3.
    - [Release notes](https://github.com/vercel/next.js/releases)
    - [Changelog](https://github.com/vercel/next.js/blob/canary/release.js)
    - [Commits](https://github.com/vercel/next.js/compare/v13.2.1...v13.2.3)
    
    ---
    updated-dependencies:
    - dependency-name: next
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * added some be
    
    * api env
    
    * api test
    
    * api test
    
    * changed api
    
    * test
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- next/src/components/Projects/Zoeschinger/index.tsx
+++ next/src/components/Projects/Zoeschinger/index.tsx
@@ -1,10 +1,12 @@
 import styles from '@s/projects/zoeschinger/main.module.scss';
+import Link from 'next/link';
 
 const Zoeschinger = () => {
   return (
     <>
       <div className={styles.main}>
         <h1>This website</h1>
+        <Link href={'/projects/zoeschinger/reqtest'}>to req testing</Link>
       </div>
     </>
   );
---
+++ next/src/components/ReqTest/index.tsx
@@ -0,0 +1,33 @@
+const getServerJSON = async () => {
+  const ip = process.env.PRIVATE_API_ADDRESS;
+  const api = ip + 'api/v1/users';
+  console.log(api);
+
+  const res = await fetch(api);
+  const json = await res.json();
+
+  return json;
+};
+
+const ReqTest = () => {
+  const handlePOST = () => {
+    console.log('POST');
+  };
+
+  const handleGET = () => {
+    getServerJSON().then((json) => {
+      console.log(json);
+    });
+  };
+
+  return (
+    <>
+      <div>
+        <button onClick={handlePOST}>POST</button>
+        <button onClick={handleGET}>GET</button>
+      </div>
+    </>
+  );
+};
+
+export default ReqTest;
