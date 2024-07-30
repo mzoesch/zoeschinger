324d1c19182c19e676b128e04487531d1dec0f91
mzoesch <magnus.zoeschinger@zoeschinger.de>
2024-07-30 16:34:53 +0200

    Adds more querying

--- app/cmain.py
+++ app/cmain.py
@@ -1,9 +1,11 @@
 import lib.c24hc_main as c24hc_main
 
+
 def main():
     data = c24hc_main.main(root_file=__file__)
     res = data.get_cheapest_offers()
     print('res ', res)
+
 
 if __name__ == '__main__':
     main()
